a9827badff9e31370d441812205fe0351f745d75
Brandon Ashby <bash7325@gmail.com>
2022-08-31 19:09:50 -0600

    new folder structure for express

---
+++ public/assets/js/script.js
@@ -0,0 +1,20 @@
+// when yes button is clicked, increase count by 1
+const yesButton = document.getElementById('yes');
+yesButton.addEventListener('click', function() {
+    event.preventDefault();
+    const count = document.getElementById('count');
+    const countValue = parseInt(count.innerText);
+    count.innerText = countValue + 1;
+    console.log(countValue);
+} );
+
+
+// when no button is clicked, increase no count by 1
+const noButton = document.getElementById('no');
+noButton.addEventListener('click', function() {
+    event.preventDefault();
+    const count = document.getElementById('noCount');
+    const countValue = parseInt(count.innerText);
+    count.innerText = countValue + 1;
+    console.log(countValue);
+} );
