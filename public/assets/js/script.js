9115cba02bcdd9ff56f800da81c5ee2b44616730
Brandon Ashby <bash7325@gmail.com>
2022-09-11 15:08:22 -0600

    fixed no button

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -9,12 +9,14 @@ yesButton.addEventListener('click', function() {
 } );
 
 
-// when no button is clicked, increase no count by 1
+// when no button is clicked, decrease no count by 1
 const noButton = document.getElementById('no');
 noButton.addEventListener('click', function() {
     event.preventDefault();
-    const count = document.getElementById('noCount');
+    const count = document.getElementById('count');
     const countValue = parseInt(count.innerText);
-    count.innerText = countValue + 1;
+    count.innerText = countValue - 1;
     console.log(countValue);
-} );
+}
+);
+
